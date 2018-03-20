f88abdc0dc282722b5cb1a677806183ec077821b
TELHOOSIN-LAB-D\taher <taher.elhossin@symbyo.com>
2018-03-20 17:59:52 +0200

    refactor storage code

--- Source/Front End/phr-manager/src/utils/StorageHelper.js
+++ Source/Front End/phr-manager/src/utils/StorageHelper.js
@@ -1,7 +1,37 @@
-export function uploadFile(file){
-    return "file address";
+var ipfsAPI = require('ipfs-api');
+
+
+let ipfs = ipfsAPI('ipfs.infura.io', '5001', { protocol: 'https' });
+
+// expect file content
+export function uploadFile(fileContent) {
+
+    // let fileReader = new FileReader();
+    // fileReader.readAsBinaryString(fileContent)
+    //  var fileData = fileReader.result;
+
+    return new Promise((resolve, reject) => {
+        // fileReader.onload = () => {
+        //     let data = fileReader.result;
+        //     let buffer = Buffer.from(data);
+        //     let content = []
+        //     content.push({
+        //         path: "file 1",
+        //         content: buffer
+        //     });
+
+            ipfs.add(content, (err, res) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(res[0].hash);
+            });
+       // }
+     });
 }
 
-export function downloadFile(fileAddress){
-    return "the file";
-}
+
+
+        export function downloadFile(fileAddress) {
+            return "the file";
+        }
